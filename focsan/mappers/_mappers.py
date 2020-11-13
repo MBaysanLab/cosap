28f2c56a6023b73e9f5f38dde9093e4d99e2da01
omercinal <omercinal@gmail.com>
2020-11-13 20:18:29 +0300

    changes

--- focsan/mappers/_mappers.py
+++ focsan/mappers/_mappers.py
@@ -19,28 +19,28 @@ class _Mappable:
     def _get_file_information(
         cls, fastq_list: List, sample_type: str, trimmed: bool = False
     ) -> Dict:
-        splitted_files = list(
-            map(lambda x: x.replace(".fastq.gz", "").split("_"), fastq_list)
-        )
+        format_string = ""
+        if trimmed:
+            format_string = "{Drop}_"
 
-        # Picks the correct dict keys in order
         if sample_type == "tumor":
-            keys = ("Sample_ID", "Index", "Lanes", "Pairs", "Number_of_seq")
+            format_string += "{Sample_ID}_{Index}_{Lanes}_{Pairs_Number_of_seq}.fastq.gz"
         elif sample_type in ("germline", "normal"):
-            keys = ("Sample_ID", "Germline", "Index", "Lanes", "Pairs", "Number_of_seq")
+            format_string += "{Sample_ID}_{Germline}_{Index}_{Lanes}_{Pairs_Number_of_seq}.fastq.gz"
         else:
             raise Exception(f"Unknown sample type: {sample_type}")
 
-        # converts True to 1 and visa versa
-        i = int(trimmed)
+        fastq_filename_parser = Parser(format_string)
 
-        # Cleans filename string into pieces
-        values = [list(set(items[i : len(keys)])) for items in zip(*splitted_files)]
+        info_dict = defaultdict(set)
+        for filename in fastq_list:
+            file_info = fastq_filename_parser.parse(filename)
+            for key, value in file_info.items():
+                if key != "Drop":
+                    info_dict[key].add(value)
 
-        # creates a dict by the order
-        info_dict = dict(zip(keys, values))
+        return dict(map(lambda x: (x[0], list(x[1]), info_dict.items())))
 
-        return info_dict
 
 
 class BWAMapper(_Mapper, _Mappable):
