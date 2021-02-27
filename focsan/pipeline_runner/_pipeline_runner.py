43816b22821e508b928936ff12b9f11fcd2ffe02
omercinal <omercinal@gmail.com>
2021-02-27 13:47:32 +0300

    adding mapping

--- focsan/pipeline_runner/_pipeline_runner.py
+++ focsan/pipeline_runner/_pipeline_runner.py
@@ -1,10 +1,13 @@
 from typing import Dict, List
-from ._keys import PipelineKeys
+from ._keys import PipelineKeys, MappingKeys
+from ..mappers import MapperFactory
 
 
 class PipelineRunner:
     def map(self, mapping_configs: List):
-        pass
+        for config in mapping_configs:
+            mapper = MapperFactory.create(config[MappingKeys.LIBRARY])
+            mapper.map(config)
 
     def sort(self, sorting_configs: List):
         pass
