11a1cd65cdcee0d4e3efa435d2d0dd2dd38e2de5
Omer <omercinal@gmail.com>
2021-02-27 14:15:58 +0300

    adding caller loop in pipeline

--- focsan/pipeline_runner/_pipeline_runner.py
+++ focsan/pipeline_runner/_pipeline_runner.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
 from ._keys import PipelineKeys, MappingKeys
 from ..mappers import MapperFactory
+from ..variant_callers import VariantCallerFactory
 
 
 class PipelineRunner:
@@ -13,7 +14,9 @@ class PipelineRunner:
         pass
 
     def call_variants(self, variant_configs: List):
-        pass
+        for config in variant_configs:
+            caller = VariantCallerFactory.create(config[VariantCallingKeys.LIBRARY])
+            caller.call_variants(config)
 
     def run_pipeline(self, pipeline_config: Dict):
         self.validate_pipeline_config(pipeline_config)
