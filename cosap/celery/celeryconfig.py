9abcf6651bc3e900959c8d9aa7892e3bff595ec9
mae <mehmetarifergun@gmail.com>
2023-03-27 04:32:14 +0300

    elprep known-sites

--- cosap/celery/celeryconfig.py
+++ cosap/celery/celeryconfig.py
@@ -1,3 +1,3 @@
-broker_url = "amqp://guest:guest@localhost:5672/vhost"
-result_backend = "redis://localhost:6379/0"
+broker_url = "amqp://guest:guest@rabbitmq:5672/vhost"
+result_backend = "redis://redis:6379/0"
 task_track_started = True
--- cosap/preprocessors/_elprep_preprocess.py
+++ cosap/preprocessors/_elprep_preprocess.py
@@ -21,6 +21,8 @@ class ElprepPreprocess(_Preprocessor, _PreProcessable):
             f"{elprep_config[ElprepKeys.OUTPUT]}_metrics",
             "--bqsr",
             elprep_config[ElprepKeys.TABLE],
+            "--known-sites",
+            ",".join([library_paths.DBSNP, library_paths.MILLS_INDEL, library_paths.ONE_THOUSAND_G]),
             "--reference",
             library_paths.REF_ELFASTA,
         ]
